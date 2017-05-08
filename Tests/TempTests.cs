3c9ac686607c6f376ed794362de05dad28bfe572
Sava Lifanov <wexxion@gmail.com>
2017-05-08 22:34:52 +0500

    NUnit test project added

---
+++ Tests/TempTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    public class BlaBlaCommand 
+    {
+        
+    }
+
+    [TestClass]
+    public class TempTests
+    {
+        [TestMethod]
+        public void ReflectionTest()
+        {
+           
+        }
+    }
+}
