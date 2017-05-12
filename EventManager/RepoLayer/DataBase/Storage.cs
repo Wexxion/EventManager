535e11c7b5f4814db8467d79df6715f226080896
Andrey <mopozko123@gmail.com>
2017-05-12 15:32:50 +0500

    rename

--- EventManager/RepoLayer/DataBase/Storage.cs
+++ EventManager/RepoLayer/DataBase/Storage.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using AutoMapper;
+using TaskManager.DomainLayer;
+using TaskManager.RepoLayer.DataBase.DbModel;
 
 namespace TaskManager.RepoLayer.DataBase
 {
-    class Storage
+    public class Storage
     {
+        private Context Db { get; }
+        public Storage()
+        {
+            this.Db = new Context("TestBot");
+        }
+
+        public void AddUser(Person user)
+        {
+            var dbUser = Db.Users.Find(user.TelegramId);
+            if (dbUser == null)
+            { 
+                Db.Users.Add(Mapper.Map<PersonInDb>(user));
+                Db.SaveChanges();
+            }
+        }
+        public void AddEvent(VEvent e)
+        {
+            var dbUser = GetUser(e.Creator.TelegramId);
+            var dbEvent = Mapper.Map<VEventInDb>(e);
+            dbEvent.Creator = dbUser;
+            foreach (var user in e.Participants)
+                dbEvent.Participants.Add(GetUser(user.TelegramId));
+            Db.Events.Add(dbEvent);
+            Db.SaveChanges();
+        }
+        public PersonInDb GetUser(int id)
+        {
+            return Db
+                 .Users
+                 .Find(id);
+        }
+        public List<VEvent> GetAllUserEvent(int telegramId)
+        {
+            return Db.Events
+                .AsEnumerable()
+                .Where(x => x.Creator.TelegramId == telegramId)
+                .Select(Mapper.Map<VEvent>)
+                .ToList();
+        }
+
     }
 }
